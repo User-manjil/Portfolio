2e82ab08695c5e882969e600ea16e6e0d415c85c
Manjil <aryalmanzeel1@gmail.com>
2024-04-12 11:40:17 +0545

    first_commit

---
+++ script.js
@@ -0,0 +1,24 @@
+let slideIndex = 0;
+showSlides(slideIndex);
+
+function nextSlide() {
+    showSlides(slideIndex += 1);
+}
+
+function prevSlide() {
+    showSlides(slideIndex -= 1);
+}
+
+function showSlides(n) {
+    const slides = document.getElementsByClassName("project");
+    if (n >= slides.length) {
+        slideIndex = 0;
+    }
+    if (n < 0) {
+        slideIndex = slides.length - 1;
+    }
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slides[slideIndex].style.display = "block";
+}
